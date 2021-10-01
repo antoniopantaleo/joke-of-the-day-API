c22d1b22b6a2adcbfe9a9772b25e7b6364f4ff06
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2021-10-01 17:23:26 +0200

    clean git cache

--- dist/prisma/util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandom = void 0;
-function getRandom(array) {
-    return array[Math.floor(Math.random() * array.length)];
-}
-exports.getRandom = getRandom;
-//# sourceMappingURL=util.js.map
